8342cc0ae6288b04d94c165aa3f6eea2e64b3d52
umbi <umbi@linkingtechnologies.it>
2025-01-26 22:49:42 +0100

    Update build-segreteria-campo.sh

--- build-segreteria-campo.sh
+++ build-segreteria-campo.sh
@@ -3,6 +3,18 @@
 # Set security options to stop the script in case of an error
 set -e
 
+# Check if required commands are installed
+REQUIRED_COMMANDS=("wget" "unzip" "sed" "jq" "git" "lftp" "sshpass" "sftp" "zip")
+for cmd in "${REQUIRED_COMMANDS[@]}"; do
+  if ! command -v $cmd &> /dev/null; then
+    echo "Error: Required command '$cmd' is not installed."
+    echo "If you are using WSL (Ubuntu/Debian), install it by running:"
+    echo "  sudo apt update && sudo apt install -y $cmd"
+    exit 1
+  fi
+done
+
+
 # Variable definitions
 PHP_VERSION="8.3.16"
 BUILD_PATH="build/html"
@@ -11,6 +23,7 @@ APP_NAME="segreteriacampo"
 PLUGIN_NAME="segreteria-campo"
 LOCALE="it"
 ENVIRONMENT="local"
+ZIP_FILE="${PLUGIN_NAME}-$(date +%Y-%m-%d).zip"
 
 # Execute the required commands
 echo "Building local PHP server..."
@@ -31,5 +44,13 @@ echo "Initializing Camila App..."
 echo "Initializing Camila App Plugin..."
 ./init-camila-app-plugin.sh $BUILD_PATH $APP_NAME $PLUGIN_NAME $LOCALE $ENVIRONMENT
 
+
+echo "Creating ZIP archive: $ZIP_FILE"
+if zip -r "$ZIP_FILE" build/; then
+    echo "✅ Archive created successfully: $ZIP_FILE"
+else
+    echo "❌ Failed to create archive." >&2
+    exit 1
+fi
 # Completion message
 echo "All tasks completed successfully!"
