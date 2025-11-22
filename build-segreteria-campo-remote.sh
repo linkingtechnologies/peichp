c8cb3f30012f2316d9a3304106c4bf4e59817860
umbi <umbi@linkingtechnologies.it>
2025-11-22 11:04:47 +0100

    Script update

---
+++ build-segreteria-campo-remote.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+APP_ID=segreteriacampo
+LOCALE=it
+APP_NAME="ProtezioNET - Segreteria Campo"
+APP_TITLE="Segreteria campo"
+APP_GROUP="ProtezioNET"
+PLUGIN_NAMES="segreteria-campo"
+DB_DSN=mysql://<utente>:<password>@<host>:<porta>/<nome_database>
+
+./build-worktable-app.sh "remote" "$APP_ID" "$LOCALE" "$APP_NAME" "$APP_TITLE" "$APP_GROUP" "$PLUGIN_NAMES" "$DB_DSN"
